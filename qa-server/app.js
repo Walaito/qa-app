22b533c1ee5176f96c7928ef82c1d086c72b74e6
Walai <eaawato@students.eaaa.dk>
2020-06-06 12:19:59 +0200

    Specify env using ATLAS_URI

--- qa-server/app.js
+++ qa-server/app.js
@@ -70,7 +70,7 @@ app.get('*', (req, res) =>
 );
 
 /**** Start! ****/
-const url = process.env.MONGO_URL || 'mongodb://localhost/question_db';
+const url = process.env.ATLAS_URI || 'mongodb://localhost/question_db';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(async () => {
         //  await questionDB.bootstrap(); // Fill in test data if needed.
